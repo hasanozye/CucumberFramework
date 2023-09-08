45bc25d4c3bc2cf5bc5413fe2628ce41640c31be
Hasan Ozyer <hasanozyer06@gmail.com>
2023-09-08 17:57:46 +0300

    Implemented reusable base steps class for Cucumber step definitions with Selenium WebDriver, providing methods for interacting with elements including handling click actions, sending keys, and waiting for element visibility.

---
+++ MyWork/src/test/java/stepdefs/BaseSteps.java
@@ -0,0 +1,103 @@
+package stepdefs;
+
+import driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseSteps {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    protected BaseSteps() {
+        driver = Driver.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void click(WebElement element) {
+//        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        /*
+        wait.until(driver1 -> {
+            try {
+                element.clear();
+                element.sendKeys(text);
+                return true;
+            } catch (Exception e1) {
+            return false;
+            }
+       });
+         */
+        wait.until(driver1 -> {
+            try {
+                element.click();
+                return true;
+            } catch (Exception e1) {
+                try {
+                    new Actions(driver1)
+                            .moveToElement(element)
+                            .click()
+                            .perform();
+                    return true;
+                } catch (Exception e2) {
+                    try {
+                        ((JavascriptExecutor) driver1).executeScript("arguments[0].click()", element);
+                        return true;
+                    } catch (Exception e3) {
+                        return false;
+                    }
+                }
+            }
+        });
+    }
+
+    public void click(By locator) {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        click(element);
+    }
+
+    public void sendKeys(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        sendKeys(element, text);
+    }
+
+    public void sendKeys(WebElement element, String text) {
+
+        wait.until(driver1 -> {
+            try {
+                element.clear();
+                element.sendKeys(text);
+                return true;
+            } catch (Exception e1) {
+                try {
+                    element.clear();
+                    new Actions(driver1)
+                            .moveToElement(element)
+                            .sendKeys(text)
+                            .perform();
+                    return true;
+                } catch (Exception e2) {
+                    try {
+                        element.clear();
+                        ((JavascriptExecutor) driver1).executeScript("arguments[0].value()'" + text + "'", element);
+                        return true;
+                    } catch (Exception e3) {
+                        return false;
+                    }
+                }
+            }
+        });
+    }
+
+    public void waitForVisibility(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+
+}
