a62ce17e963639501b9143025ca34422f30922b1
Hasan Ozyer <hasanozyer06@gmail.com>
2023-09-11 22:02:29 +0300

    added plugins for json html and aventstack reports

--- MyWork/src/test/java/runners/RunnerLogin.java
+++ MyWork/src/test/java/runners/RunnerLogin.java
@@ -5,7 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/resources/features/Login.feature"},
-        glue ={"stepdefs"}
+        glue = {"stepdefs"},
+        plugin = {"pretty",
+                "json:test-output/cucumber-reports/cucumber.json",
+                "html:test-output/cucumber-reports/cucumberreport.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class RunnerLogin extends AbstractTestNGCucumberTests {
 }
