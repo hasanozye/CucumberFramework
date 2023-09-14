e94e2a8b3ddc274b63cec81945408453e61ff37e
Hasan Özyer <hasanozyer06@gmail.com>
2023-09-14 21:02:43 +0300

    just small change about learning regex method in string

--- MyWork/src/main/java/_temp/temp1.java
+++ MyWork/src/main/java/_temp/temp1.java
@@ -4,18 +4,12 @@ import readers.property.PropertyReader;
 
 public class temp1 {
     public static void main(String[] args) {
-        String list = "--start-maximized, --remote-allow-origins=*, --headless";
+        String list = "--start-maximized  , --remote-allow-origins=*  , --headless";
 
         String[] split = list.split(",");
         for (String s : split) {
             System.out.println(s.trim() );
         }
-
-        PropertyReader pr = new PropertyReader("config");
-        pr.get("vhrome.options");
-
-        PropertyReader pr1 = PropertyReader.read("config");
-        pr1.get("chrome.options");
 
     }
 }
