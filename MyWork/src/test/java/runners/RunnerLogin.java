c4f0fd213d9034db5d96b0b2227f48e69a437184
Hasan Özyer <hasanozyer06@gmail.com>
2023-09-18 21:47:45 +0300

    Runner is changed with new features and tags.

--- MyWork/src/test/java/runners/RunnerLogin.java
+++ MyWork/src/test/java/runners/RunnerLogin.java
@@ -2,15 +2,18 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeTest;
 
 @CucumberOptions(
-        features = {"src/test/resources/features"},
+        features = {"src/test/resources/features/Wishlist.feature"},
         glue = {"stepdefs"},
-        tags = "@login2",
+        tags = "@tc3",
         plugin = {"pretty",
                 "json:test-output/cucumber-reports/cucumber.json",
                 "html:test-output/cucumber-reports/cucumberreport.html",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class RunnerLogin extends AbstractTestNGCucumberTests {
+
+
 }
