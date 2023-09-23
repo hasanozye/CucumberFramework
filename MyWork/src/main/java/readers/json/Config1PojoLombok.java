b625ddca274457196fffab916f402446fc561f08
Hasan Özyer <hasanozyer06@gmail.com>
2023-09-23 14:56:22 +0300

    Created a Java class 'Config1PojoLombok' that extends 'MyJsonPojo,' utilizing Lombok annotations to automatically generate getters, setters, and a 'toString' method for simplified code and improved maintainability.

---
+++ MyWork/src/main/java/readers/json/Config1PojoLombok.java
@@ -0,0 +1,20 @@
+package readers.json;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Config1PojoLombok extends MyJsonPojo{
+    //lombok annotationslar kullanılarak getter, setter, toString ve error islemlerinde kolaylik saglar
+    //Plain Old Java Objects
+
+
+    private String url;
+    private String username;
+    private String password;
+
+
+}
