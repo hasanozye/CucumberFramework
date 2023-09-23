509eb80cc5756681b644693533b2d22c2d72d35e
Hasan Özyer <hasanozyer06@gmail.com>
2023-09-23 14:57:54 +0300

    Implemented test cases for reading and parsing JSON configuration files using the JSON-Simple library and the JSONParser, demonstrating the extraction of values, arrays, and nested objects for configuration and data retrieval in test scenarios.

---
+++ MyWork/src/main/java/readers/json/TestReadJson.java
@@ -0,0 +1,54 @@
+package readers.json;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class TestReadJson {
+
+    @Test
+    public void readConfig1() throws Exception {
+
+        //okunacak json dosyası
+        String file = "src/test/resources/datafiles/Config1.json";
+
+        // Config1.json JSONObject olarak okunuyor.
+//        JSONObject object = (JSONObject) JSONValue.parse(new FileReader(file));
+        JSONObject object = (JSONObject) new JSONParser().parse(new FileReader(file));
+        System.out.println(object);
+        System.out.println("-".repeat(25));
+        System.out.println(object.get("url"));
+        System.out.println(object.get("username"));
+        System.out.println(object.get("password"));
+
+    }
+
+    @Test
+    public void readConfig2() throws Exception {
+        String file = "src/test/resources/datafiles/Config2.json";
+        JSONObject json = (JSONObject) new JSONParser().parse(new FileReader(file));
+        System.out.println(json.get("types"));
+
+        // array olan type değerini JSONArray'a atıyorum
+        JSONArray types = (JSONArray) json.get("types");
+        for (var type : types) {
+            System.out.println(type);
+        }
+
+        System.out.println("-".repeat(32));
+        JSONArray users = (JSONArray) json.get("users");
+
+
+
+        for (Object user : users) {
+            JSONObject userObj = (JSONObject) user;
+            System.out.println(userObj.get("username"));
+        }
+
+    }
+}
