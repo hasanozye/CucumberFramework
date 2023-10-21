d8d1a9658b2f04b696e7e5ae6095fdf05b23dab3
Hasan Özyer <hasanozyer06@gmail.com>
2023-10-21 17:55:25 +0300

    added new comment lines

--- MyWork/src/test/java/_temp/JsonReadClass.java
+++ MyWork/src/test/java/_temp/JsonReadClass.java
@@ -9,12 +9,14 @@ import java.io.FileReader;
 
 public class JsonReadClass {
     public static void main(String[] args) throws FileNotFoundException {
+
+        // 1. Define the file path for the JSON file containing the data.
         String jsonFile = "MyWork/src/test/resources/datafiles/Elements.json";
 
-//         Json dosyasini ilk defa okumak
+        // 2. Read the JSON file for the first time and parse it into a JSONObject.
         JSONObject object = (JSONObject) JSONValue.parse(new FileReader(jsonFile));
 
-//        JSON içerisinde il key okunur, value { } içindeyse bu da objecttir.
+        // 3. Retrieve the 'myaccount' object from the parsed JSON.
         JSONObject myaccount = (JSONObject) object.get("myaccount");
 
         String type = myaccount.get("type").toString();
@@ -22,33 +24,33 @@ public class JsonReadClass {
         System.out.println(getBy(type, locator));
     }
 
-    public static By getBy(String type, String locator) {
+        public static By getBy(String type, String locator) {
 
-        switch (type) {
-            case "xpath" -> {
-                return By.xpath(locator);
-            }
-            case "css" -> {
-                return By.cssSelector(locator);
-            }
-            case "id" -> {
-                return By.id(locator);
-            }
-            case "tagname" -> {
-                return By.tagName(locator);
-            }
-            case "classname" -> {
-                return By.className(locator);
-            }
-            case "linktext" -> {
-                return By.linkText(locator);
-            }
-            case "partiallinktext" -> {
-                return By.partialLinkText(locator);
-            }
-            default -> {
-                return null;
+            switch (type) {
+                case "xpath" -> {
+                    return By.xpath(locator);
+                }
+                case "css" -> {
+                    return By.cssSelector(locator);
+                }
+                case "id" -> {
+                    return By.id(locator);
+                }
+                case "tagname" -> {
+                    return By.tagName(locator);
+                }
+                case "classname" -> {
+                    return By.className(locator);
+                }
+                case "linktext" -> {
+                    return By.linkText(locator);
+                }
+                case "partiallinktext" -> {
+                    return By.partialLinkText(locator);
+                }
+                default -> {
+                    return null;
+                }
             }
         }
-    }
 }
