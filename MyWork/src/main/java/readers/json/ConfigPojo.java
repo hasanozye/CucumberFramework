8187940da8f72349fa567aa423752662eaf1b617
Hasan Özyer <hasanozyer06@gmail.com>
2023-10-21 17:55:06 +0300

    Added Address Class with object for Lombok Annotation processing.

--- MyWork/src/main/java/readers/json/ConfigPojo.java
+++ MyWork/src/main/java/readers/json/ConfigPojo.java
@@ -13,18 +13,31 @@ public class ConfigPojo extends MyJsonPojo {
     private String url;
     private String username;
     private String password;
-    private String takescreenshot;
+    private boolean takescreenshot;
     private String takescreenshotOn;
     private List<User> users;
+
+
 
     @Getter
     @Setter
     @ToString
     public static class User {
         private String type;
-        private String valid;
+        private boolean valid;
         private String username;
         private String password;
+        private Adress adress;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Adress{
+        private String city;
+        private String cadde;
+        private int numara;
+
     }
 
 
