5f120113ce990d670561248d0a3845fa537a9837
Hasan Özyer <hasanozyer06@gmail.com>
2023-10-22 18:15:22 +0300

    optimized sub-optimal code

--- MyWork/src/main/java/readers/json/TestMapper.java
+++ MyWork/src/main/java/readers/json/TestMapper.java
@@ -3,11 +3,11 @@ package readers.json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static utils.Utils.*;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestMapper {
 
@@ -31,7 +31,8 @@ public class TestMapper {
     public void testMapConfig1LomBok() throws Exception {
         String file = "src/test/resources/datafiles/Config1.json";
         ObjectMapper mapper = new ObjectMapper();
-        Config1PojoLombok config = mapper.readValue(new FileReader(file), Config1PojoLombok.class);
+        Config1PojoLombok config = mapper
+                .readValue(new FileReader(file), Config1PojoLombok.class);
         System.out.println(config);
         System.out.println("-".repeat(30));
         System.out.println(config.getUrl());
@@ -41,45 +42,41 @@ public class TestMapper {
 
     @Test
     public void testMapConfig() throws Exception {
-        String file = "src/test/resources/datafiles/configders.json";
+        String file = "src/test/resources/datafiles/config.json";
         ObjectMapper mapper = new ObjectMapper();
         ConfigPojo config = mapper.readValue(new FileReader(file), ConfigPojo.class);
         System.out.println(config);
-        System.out.println(config.getUsers().get(0).getPassword()  );
+        System.out.println(config.getUsers().get(0).getPassword());
         System.out.println("-".repeat(30));
     }
 
     @Test
-    public void testMapGeneral1(){
+    public void testMapGeneral1() {
         String file = "src/test/resources/datafiles/Config1.json";
 
-//        ObjectMapper mapper = new ObjectMapper();
         MyJsonPojo pojo = new Config1PojoLombok();
-//        MyJsonPojo pojo2 = new ConfigPojo();
-
-        Config1PojoLombok data = (Config1PojoLombok) getPojo(file,pojo);
+        Config1PojoLombok data = (Config1PojoLombok) getPojo(file, pojo);
         System.out.println(data.getUrl());
     }
 
     @Test
-    public void testMapGeneral2(){
+    public void testMapGeneral2() {
         String file = "src/test/resources/datafiles/config.json";
 
-//        ObjectMapper mapper = new ObjectMapper();
         MyJsonPojo pojo = new ConfigPojo();
-//        MyJsonPojo pojo2 = new ConfigPojo();
-
-        ConfigPojo data = (ConfigPojo) getPojo(file,pojo);
-//        System.out.println(data.getUsers().get(0).getAddress().getCity());
+        ConfigPojo data = (ConfigPojo) getPojo(file, pojo);
+        System.out.println("data.getUsers().get(0).getAdress().getCity() = " + data
+                .getUsers()
+                .get(0)
+                .getAdress()
+                .getCity());
     }
 
-    public Object getPojo(String file, MyJsonPojo pojo){
+    @Test
+    public MyJsonPojo t1(String jsonFile, MyJsonPojo pojo) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(new FileReader(file),pojo.getClass());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return mapper.readValue(new FileReader(jsonFile), pojo.getClass());
+
     }
 
 
