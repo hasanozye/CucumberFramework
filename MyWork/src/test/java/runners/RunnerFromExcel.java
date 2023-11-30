3c9f4a1eae1497eec14bb09af547ccb4e290a8be
Hasan Özyer <hasanozyer06@gmail.com>
2023-11-30 17:51:06 +0300

    called export xml from utils file.

--- MyWork/src/test/java/runners/RunnerFromExcel.java
+++ MyWork/src/test/java/runners/RunnerFromExcel.java
@@ -3,11 +3,11 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeTest;
+import utils.Utils;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/testCaseFromExcel.feature"},
+        features = {"src/test/resources/features/featuresfromexcel"},
         glue = {"stepdefs"},
-        tags = "@tc5",
         plugin = {"pretty",
                 "json:test-output/cucumber-reports/cucumber.json",
                 "html:test-output/cucumber-reports/cucumberreport.html",
@@ -18,13 +18,7 @@ public class RunnerFromExcel extends AbstractTestNGCucumberTests {
     @BeforeTest
     public void beforeTest() {
         String fileExcel = "src/test/resources/datafiles/testCase.xlsx";
-        String fileFeature = "src/test/resources/features/testCaseFromExcel.feature";
-        createFeatureFile(fileExcel, fileFeature);
+        String fileFeature = "src/test/resources/features/featuresfromexcel/testCaseFromExcel.feature";
+        Utils.createFeatureFileFromExcel(fileExcel, fileFeature);
     }
-
-    public void createFeatureFile(String fileExcel, String feature) {
-        // fileExcel'deki senaryoyu fileFeature'a yazacak.
-    }
-
-
 }
